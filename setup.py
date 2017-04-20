a722991273aa22003b78dbd6531d73004a3fe911
Sergei <hacker.mad.cat@gmail.com>
2017-04-20 16:35:42 +0300

    Fixed: installation bags

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='live-plotter',
     version='1.0',
-    packages=['live_plotter'],
+    packages=['live_plotter', 'live_plotter.proxy'],
     url='https://github.com/DeveloperHacker/Live-Plotter',
     license='MIT',
     author='HackerMadCat',
